942a3dbfe546a715d7e7d985327ff0e4a33e8052
Jeff Schwab <jeff@unbuggy.com>
2022-02-01 23:24:50 -0500

    Import wasm; broken by some Wasm/ESM/WebPack issue

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  import('wasm').then(wasm => {
+    console.log(wasm);
+  });
   return (
     <div className="App">
       <header className="App-header">
